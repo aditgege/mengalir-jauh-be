17081d4783fcfefce10f7d377f3e72aa762de2f3
aditgege <aditgege0@gmail.com>
2022-04-03 22:10:49 +0700

    change to supabase

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -9,10 +9,20 @@ module.exports = ({ env }) => ({
       database: config.database,
       user: config.user,
       password: config.password,
-      ssl: {
-        rejectUnauthorized: false
-      },
     },
-    debug: false,
   },
 });
+// const path = require('path');
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: env('DATABASE_HOST', '127.0.0.1'),
+//       port: env.int('DATABASE_PORT', 5432),
+//       database: env('DATABASE_NAME', 'postgres'),
+//       user: env('DATABASE_USERNAME', 'postgres'),
+//       password: env('DATABASE_PASSWORD', 'root'),
+//     },
+//   },
+// });
