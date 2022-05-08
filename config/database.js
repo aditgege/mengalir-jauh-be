ebefc8c2d139cf3873203507f8641ef67e1aa721
aditgege <aditgege0@gmail.com>
2022-05-08 21:33:46 +0700

    fix transaction relation

--- config/database.js
+++ config/database.js
@@ -1,14 +1,29 @@
-const path = require('path');
-
-module.exports = ({ env }) => ({
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.SUPABASE_URL);
+console.log(process.env.APP_KEYS)
+module.exports = ({ _env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'postgres'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'root'),
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
     },
   },
 });
+// const path = require('path');
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: env('DATABASE_HOST', '127.0.0.1'),
+//       port: env.int('DATABASE_PORT', 5432),
+//       database: env('DATABASE_NAME', 'postgres'),
+//       user: env('DATABASE_USERNAME', 'postgres'),
+//       password: env('DATABASE_PASSWORD', 'root'),
+//     },
+//   },
+// });
